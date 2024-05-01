c0de88cdafe18f21977a2931e39c9d379efd1d11
yamashitafumihiro <fumiwata2000@gmail.com>
2024-05-01 23:43:50 +0900

    ルーティング設定

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {Typography} from "@mui/material";
+
+const Home: React.FC = () => {
+    return (
+        <Typography>Home</Typography>
+    );
+};
+
+export default Home;
