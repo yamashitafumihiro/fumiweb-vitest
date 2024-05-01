aa98855cc181386abbb87d958fae222a9fda1d1a
yamashitafumihiro <fumiwata2000@gmail.com>
2024-05-01 23:27:51 +0900

    setup vite and MUI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,9 +17,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button>test button</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
