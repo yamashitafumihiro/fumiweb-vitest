7b531e55e3966238de9d92ee96d6b6740a5c4b56
yamashitafumihiro <fumiwata2000@gmail.com>
2024-05-01 23:56:51 +0900

    ルーティング

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,22 +4,22 @@ import React, {Suspense} from "react";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Home from "./components/Home.tsx";
 
-const Loading: React.FC =()=> {
+const Loading: React.FC = () => {
     return (
-        <Box sx={{width:'100%'}}>
-            <LinearProgress />
+        <Box sx={{width: '100%'}}>
+            <LinearProgress/>
             <Typography role="paragraph">Loading...</Typography>
         </Box>
     );
 };
 
-const App:React.FC = () => {
-    return(
-        <Suspense fallback={<Loading />}>
+const App: React.FC = () => {
+    return (
+        <Suspense fallback={<Loading/>}>
             <BrowserRouter basename="/app">
                 <Routes>
-                    <Route path="/" element={<Navigate to="/home" replace />}></Route>
-                    <Route path="/home" element={<Home />}></Route>
+                    <Route path="/" element={<Navigate to="/home" replace/>}></Route>
+                    <Route path="/home" element={<Home/>}></Route>
                 </Routes>
             </BrowserRouter>
         </Suspense>
