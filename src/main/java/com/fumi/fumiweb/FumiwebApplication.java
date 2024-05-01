fe6d462c702fc05c2c41798408ff7ed064ce3f73
yamashitafumihiro <fumiwata2000@gmail.com>
2024-05-01 23:13:22 +0900

    init commit

---
+++ src/main/java/com/fumi/fumiweb/FumiwebApplication.java
@@ -0,0 +1,13 @@
+package com.fumi.fumiweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FumiwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FumiwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fumi/fumiweb/FumiwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fumi.fumiweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FumiwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
