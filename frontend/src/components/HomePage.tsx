c8c509392c34da63288fba72e316adc2ce43fdaf
yamashitafumihiro <fumiwata2000@gmail.com>
2024-08-01 16:14:55 +0900

    次ページへのボタンと前ページへのボタンの間のページ数を指定するボタンの削除

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -3,7 +3,6 @@ import {
     Box,
     Button,
     Grid, Stack,
-    TextField,
     Typography,
 } from "@mui/material";
 import {useWindowSize} from "../hooks/useWindowSize.tsx";
@@ -24,7 +23,6 @@ const HomePage: React.FC = () => {
 
     const postsPerPage = 4;
     const maxPages = Math.ceil(blogposts.length / postsPerPage);
-    const [inputPage, setInputPage] = useState<null | number>(null);
     const [currentPage, setCurrentPage] = useState<number>(1);
     const indexOfLastPost = currentPage * postsPerPage;
     const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -36,11 +34,6 @@ const HomePage: React.FC = () => {
 
     const handleNextPage = () => {
         setCurrentPage(prev => Math.min(prev + 1, maxPages));
-    };
-
-    const handleMoveClick = () => {
-        const targetPage = Number(inputPage) ? Math.max(1, Math.min(Number(inputPage), maxPages)) : 1;
-        setCurrentPage(targetPage);
     };
 
     return (
@@ -77,31 +70,15 @@ const HomePage: React.FC = () => {
                         </Link>
                     </Box>
                 ))}
-                <Grid container alignItems='center' margin={4}>
-                    <Grid item xs={2}></Grid>
-                    <Grid item xs={2}>
+                <Grid container alignItems='center'>
+                    <Grid item xs={6}>
                         <Button variant="outlined" size="large" onClick={handlePrevPage}
                                 sx={{textTransform: "none"}}>prev</Button>
                     </Grid>
-                    <Grid item xs={2}>
-                        <TextField
-                            label="page"
-                            variant="outlined"
-                            type="number"
-                            size="small"
-                            value={inputPage || ''}
-                            onChange={(e) => setInputPage(Number(e.target.value))}
-                        />
-                    </Grid>
-                    <Grid item xs={2}>
-                        <Button variant="outlined" onClick={() => handleMoveClick()}
-                                disabled={!inputPage || inputPage > maxPages} sx={{textTransform: "none"}}>move</Button>
-                    </Grid>
-                    <Grid item xs={2}>
+                    <Grid item xs={6}>
                         <Button variant="outlined" size="large" onClick={handleNextPage} sx={{textTransform: "none"}}>next
                         </Button>
                     </Grid>
-                    <Grid item xs={2}></Grid>
                 </Grid>
             </Box>
         </Box>
