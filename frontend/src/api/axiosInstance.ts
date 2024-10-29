3a10583009f89022fb58a8a5e98f03091564ccc3
yamashitafumihiro <fumiwata2000@gmail.com>
2024-10-29 19:34:16 +0900

    api対応

---
+++ frontend/src/api/axiosInstance.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseURL = import.meta.env.VITE_API_URL;
+
+const axiosInstance = axios.create({
+    baseURL,
+    headers:{
+        'Content-Type':'application/json',
+    },
+});
+
+export default axiosInstance
